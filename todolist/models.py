b0b0d40fa11dd5436c4fe28e334e65d051705aa9
Vicky Maulana <muhammad.vicky@ui.ac.id>
2022-09-27 15:06:00 +0700

    task 4 complete

---
+++ todolist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Task(models.Model):
+    # user yang membuat task
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)  # tanggal dibuat
+    title = models.CharField(max_length=100)  # judul task
+    description = models.TextField()  # deskripsi task
+    is_finished = models.BooleanField(default=False)  # status task
---
+++ todolist/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from todolist.models import Task
+
+# Create your views here.
+
+
+@login_required(login_url='todolist:login')
+def show_todolist(request):
+    data_task = Task.objects.filter(user=request.user)
+    context = {
+        'task_list': data_task,
+        'nama': request.user.username,
+        'npm': '2106750906',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, 'todolist.html', context)
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login')
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)  # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(
+                reverse("todolist:show_todolist"))  # membuat response
+            # membuat cookie last_login dan menambahkannya ke dalam response
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('todolist:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
+@login_required(login_url='todolist:login')
+def create_task(request):
+    if request.method == "POST":
+        user = request.user
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        create_new_task = Task(user=user, title=title, description=description)
+        create_new_task.save()
+        return redirect('todolist:show_todolist')
+
+    return render(request, "create_task.html")
+
+
+def delete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('todolist:show_todolist')
+
+
+def update_task(request, id):
+    task = Task.objects.get(id=id)
+    task.is_finished = not task.is_finished
+    task.save()
+    return redirect('todolist:show_todolist')
