ffccffc4382aa07a334f2a243f500acea4e70af7
Vicky Maulana <muhammad.vicky@ui.ac.id>
2022-10-17 11:13:56 +0700

    update documentation

--- todolist/models.py
+++ todolist/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    # user yang membuat task
+    # on_delete=models.CASCADE means that if a user is deleted, all of their tasks will be deleted as well
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)  # tanggal dibuat
     title = models.CharField(max_length=100)  # judul task
